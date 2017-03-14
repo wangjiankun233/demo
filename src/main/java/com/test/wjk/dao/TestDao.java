093c3fb0ec6acf9632f6f8025d7fdf8b28bb2f2b
wangjiankun233 <wjk5442908@163.com>
2017-03-14 10:56:06 +0800

    Rejected commit: Default (2)

--- src/main/java/com/test/wjk/dao/TestDao.java
+++
@@ -1,8 +0,0 @@
-package com.test.wjk.dao;
-
-import java.util.List;
-import java.util.Map;
-
-public interface TestDao {
-   List<Map<String, Object>> getTest();
-}
--- src/main/java/com/test/wjk/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.test.wjk.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface TestService {
-	List<Map<String, Object>> getTest();
-}
